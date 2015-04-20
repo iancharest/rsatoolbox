0d41dbca9f14c805c9f37b2b314254cd374f0734
Cai Wingfield <cai@caiwingfield.com>
2015-04-20 17:33:50 +0100

    Pool beta distributions over timepoints.

--- +rsa/+meg/searchlight_dynamicGLM_source.m
+++ +rsa/+meg/searchlight_dynamicGLM_source.m
@@ -243,33 +243,52 @@ function [glmMeshPaths, lagSTCMetadatas] = searchlight_dynamicGLM_source(average
         
         if using_permutations
             
-            prints('Calculating p values based on %d permutations...');
+            prints('Calculating p values based on %d permutations.', nPermutations);
+            prints('(This will give the distribution at each vertex a total of %d values.)', nPermutations * nTimepoints_overlap);
             
-            parfor t = 1:nTimepoints_overlap
-                
-                prints('t = %d.', t);
-                
-                for v = 1:nVertices
+            % We'll pool across timepoints. So we'll make h0_betas a
+            % nModels-x-(nPermutations*nTimepoints_overlap)-sized matrix.
+            % This is based on the assumption that the distributions of
+            % beta values should be independnt of time.
+            % We may or may not want to make the same assumption about
+            % space, but we won't do that for now.
             
-                    % Calculate beta-distributions
-                    for p = 1:nPermutations
+            prints('Computing beta distrubitions over %d vertices...', nVertices);
+            parfor v = 1:nVertices
+
+                % Calculate beta-distributions
+
+                % We're storing timepoints and permutations and 
+                h0_i = 1;
+                for p = 1:nPermutations
+                    prints('Permutation %d of %d...', p, nPermutations);
+                    
+                    for t = 1:nTimepoints_overlap
+                        t_relative_to_data = t + lag_in_timepoints;
+                        
                         permuted_data_rdm = randomizeSimMat(average_slRDMs(v, t_relative_to_data).RDM);
-                        h0_betas(:, p) = glmfit( ... % (nModels, nPermutations)
+                        
+                        h0_betas(:, h0_i) = glmfit( ... % (nModels, nPermutations)
                             modelStack{t}', ...
                             permuted_data_rdm', ...
                         ...% TODO: Why are we making this assumption?
                         ...% TODO: What are the implications of this?
                             'normal');
+                        h0_i = h0_i + 1;
                     end
+                end
+            end
 
-                    % calculate p-vales
-                    for m = 1:nModels
+            % Calculate p-vales
+            parfor m = 1:nModels
+                prints('Computing p-values for model %d of %d...', m, nModels);
+                for v = 1:nVertices
+                    for t = 1:nTimepoints_overlap
                         % +1 because of that annoying forced all-1s model.
                         p_mesh(v, t, m) = 1 - portion(h0_betas(m, :), glm_mesh(v, t).betas(m + 1));
                     end
-                    
-                end%for:v
-            end%for:t
+                end
+            end
 
             % Save results
             p_file_name = sprintf('p_mesh-%sh', lower(chi));
